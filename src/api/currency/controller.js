fd8593e6c155e3a56f02883d3eceb71ec9b7ac54
manasovdan <afroprogger@gmail.com>
2017-03-14 21:00:04 +0600

    added active sources feature

--- src/api/currency/controller.js
+++ src/api/currency/controller.js
@@ -1,5 +1,5 @@
-import { updateCurrencies, currencies, getBestCurrency } from '../../services/currencies'
+import { updateCurrencies, currencies, updateBestCurrency } from '../../services/currencies'
 
 export const show = (req, res, next) => res.status(200).json(currencies);
 export const updateCurrency = (req, res, next) => updateCurrencies().then((result) => res.status(200).json(result));
-export const bestCurrency = (req, res, next) => res.status(200).json(getBestCurrency());
+export const bestCurrency = (req, res, next) => res.status(200).json(updateBestCurrency());
--- src/services/currencies/index.js
+++ src/services/currencies/index.js
@@ -1,38 +1,71 @@
 import _ from 'lodash'
 import parseFeeds from './parser'
 
+let lastParseTime = new Date();
 export let currencies = {};
 
-export function getBestCurrency() {
-  return Object.keys(currencies).reduce((result, sourceName) => {
-    const source = currencies[sourceName];
-    Object.keys(source.currencies).forEach((currencyName) => {
-      const currency = source.currencies[currencyName];
-      const currencySymbolIsNew = !{}.hasOwnProperty.call(result, currencyName);
-      if (currencySymbolIsNew) {
-        result[currencyName] = {
-          sell: { price: 0 },
-          buy: { price: Infinity }
-        }
+export let bestCurrencies = {};
+
+export function updateBestCurrency() {
+  function initActiveSourcesIfDoesntExist(result, currencyName) {
+    result.active_sources[currencyName] = result.active_sources[currencyName] || { active: 0, total: 0 };
+  }
+
+  function increaseTotalSources(result, currencyName) {
+    result.active_sources[currencyName].total += 1;
+  }
+
+  function increaseActiveSources(result, currencyName) {
+    result.active_sources[currencyName].active += 1;
+  }
+
+  function initBestCurrencyTemplateIfDoesntExist(result, currencyName) {
+    const currencySymbolIsNew = !{}.hasOwnProperty.call(result, currencyName);
+    if (currencySymbolIsNew) {
+      result[currencyName] = {
+        sell: { price: 0 },
+        buy: { price: Infinity }
       }
-      if (currency.buy < result[currencyName].buy.price) {
-        result[currencyName].buy = {
-          sourceName,
-          price: currency.buy
-        }
+    }
+  }
+
+  function checkAndUpdateBestBuyPrice(currency, result, currencyName, sourceName) {
+    if (currency.buy < result[currencyName].buy.price) {
+      result[currencyName].buy = {
+        sourceName,
+        price: currency.buy
       }
+    }
+  }
+
+  function checkAndUpdateBestSellPrice(currency, result, currencyName, sourceName){
       if (currency.sell > result[currencyName].sell.price) {
         result[currencyName].sell = {
           sourceName,
           price: currency.sell
-        }
+              }
+          }
       }
+
+  bestCurrencies = Object.keys(currencies).reduce((result, sourceName) => {
+    const source = currencies[sourceName];
+    Object.keys(source.currencies).forEach((currencyName) => {
+      const currency = source.currencies[currencyName];
+      initActiveSourcesIfDoesntExist(result, currencyName);
+      increaseTotalSources(result, currencyName); // TODO store total sources number somewhere instead of calculating
+      if (new Date(source.updated_at) < lastParseTime) return;
+      increaseActiveSources(result, currencyName);
+      initBestCurrencyTemplateIfDoesntExist(result, currencyName);
+      checkAndUpdateBestBuyPrice(currency, result, currencyName, sourceName);
+      checkAndUpdateBestSellPrice(currency, result, currencyName, sourceName);
     });
     return result;
-  }, {})
+  }, { active_sources: {} });
+  return bestCurrencies;
 }
 
 export function updateCurrencies() {
+  lastParseTime = new Date();
   return parseFeeds().then((newCurrencies) => {
     currencies = _.merge(currencies, newCurrencies);
     return currencies;
